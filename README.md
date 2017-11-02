02639b1be4807d608bc397b9d4c491530c38229f
Richard Bezerra <richardb@ciandt.com>
2017-11-01 22:35:56 -0200

    Adding log4j

--- README.md
+++ README.md
@@ -2,14 +2,10 @@
 
 ## Task list
 
-* Verify lost packet when verifying result
+* Add and justify log4J
 * Add timeout to pingers
 * Improve messages returned in ResponseMessage
-* Avoid duplicated code in ping method of ICMPPinger class
-* Improve unit tests of Reporter class
-* Add and justify log4J
 * Justify why build serialization manually
-* Fix assert of PingSchedulerTest
 * Avoid bubble exception
 * Explain why not static methods of Reporter class (TEST DRIVEN design)
 * Parameterize dependency of unit test on URL http
--- src/main/java/pingers/Application.java
+++ src/main/java/pingers/Application.java
@@ -1,7 +1,21 @@
 package pingers;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Application {
-    public static void main(String args[]) {
-        System.out.println("z");
+
+    final static Logger logger = Logger.getLogger(Application.class.getSimpleName());
+
+    public static void main(String args[]) throws IOException {
+
+        String[] hosts = ConfigReader.readAsStringArray("hosts");
+
+        PingScheduler scheduler = new PingScheduler(new Reporter());
+
+        scheduler.start(hosts);
+
+        logger.log(Level.INFO, "App started");
     }
 }
